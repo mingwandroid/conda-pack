38d4928ab2c448e241292ca546019c076fd40fef
Ray Donnelly <mingw.android@gmail.com>
2018-09-30 12:52:52 +0100

    Add PyCharm debugging relative import hack

--- conda_pack/__init__.py
+++ conda_pack/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import print_function, absolute_import
-
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+__name__='conda_pack'
 from .core import CondaEnv, File, CondaPackException, pack
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+from .cli import main
+main()
+
